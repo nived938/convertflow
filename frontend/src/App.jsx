d9b3244c52e0bb2fc9b84399d371e3ce558f5227
nived938 <nivedsreejaharidasan@gmail.com>
2026-08-21 15:44:54 +0530

    Switch ConvertFlow to public no-account experience

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,71 +1,35 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Pricing from "./pages/Donate";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+import Donate from "./pages/Donate";
 import ConversionPage from "./pages/ConversionPage";
 import About from "./pages/About";
-import ForgotPassword from "./pages/ForgotPassword";
 import NotFound from "./pages/NotFound";
-import ProtectedRoute from "./components/ProtectedRoute";
+import FAQ from "./pages/FAQ";
+import Contact from "./pages/Contact";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
+import ServicePage from "./pages/ServicePage";
+import LocationPage from "./pages/LocationPage";
+import BackendGuard from "./components/BackendGuard";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-
-        <Route
-        path="/"
-        element={<Home />}
-      />
-
-      <Route
-        path="/login"
-        element={<Login />}
-      />
-
-      <Route
-        path="/forgot-password"
-        element={<ForgotPassword />}
-      />
-
-      <Route
-        path="/register"
-        element={<Register />}
-      />
-
-      <Route
-        path="/about"
-        element={<About />}
-      />
-
-      <Route
-        path="/donate"
-        element={<Pricing />}
-      />
-
-      <Route
-        path="/convert"
-        element={<ConversionPage />}
-      />
-
-      <Route element={<ProtectedRoute />}>
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
-
-      </Route>
-
-      <Route path="*" element={<NotFound />} />
-
-      </Routes>
+      <BackendGuard>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/convert" element={<ConversionPage />} />
+          <Route path="/donate" element={<Donate />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/services/:type" element={<ServicePage />} />
+          <Route path="/locations/:location" element={<LocationPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BackendGuard>
     </BrowserRouter>
   );
 }
