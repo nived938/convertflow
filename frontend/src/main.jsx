1fa6c3eb4571fae4ff214be080bfe22d5ae1183c
nived938 <nivedsreejaharidasan@gmail.com>
2026-08-21 20:50:18 +0530

    Apply consistent content-sized widths to action buttons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./seo.css";
+import "./button-width-fix.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
