5e88724ed825ffb7d747dc83b8a8dc522f6faa71
nived938 <nivedsreejaharidasan@gmail.com>
2026-08-21 21:39:27 +0530

    Load ConvertFlow UI enhancement styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,4 +5,5 @@ import "./index.css";
 import "./seo.css";
 import "./button-width-fix.css";
 import "./growth.css";
+import "./enhancements.css";
 ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App/></React.StrictMode>);
