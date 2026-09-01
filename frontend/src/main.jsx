5c3c9f4c98006d5592fa8c6416932b556105e192
nived938 <nivedsreejaharidasan@gmail.com>
2026-09-01 10:02:26 +0530

    Load Puter.js at runtime instead of Vite HTML

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,15 @@ import "./growth.css";
 import "./enhancements.css";
 import "./scrollbar.css";
 
+// Load Puter.js at runtime so Vite does not process the external script as an HTML asset.
+if (!document.querySelector('script[data-puter-sdk="true"]')) {
+  const script = document.createElement("script");
+  script.src = "https://js.puter.com/v2/";
+  script.async = true;
+  script.dataset.puterSdk = "true";
+  document.head.appendChild(script);
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
